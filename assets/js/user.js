e0334c2cbe63a65d3bf1726a492f22d82ecd17c6
souProjet <soubougouin@gmail.com>
2024-10-24 16:10:34 +0200

    ajoute de la fonction updateAuthButton

--- assets/js/user.js
+++ assets/js/user.js
@@ -6,12 +6,15 @@ export function retrieveUser() {
     const tokenCookie = document.cookie.split('; ').find(row => row.startsWith('token='));
     const token = tokenCookie ? tokenCookie.split('=')[1] : null;
     if (!token) {
+        updateAuthButtons(-1);
         return -1;
     }
 
     // Recherche l'utilisateur par token dans le localStorage
     const users = JSON.parse(localStorage.getItem('users'));
     const user = users.find(user => user.token === token);
+
+    updateAuthButtons(user);
 
     if (!user) {
         return -1;
@@ -76,4 +79,25 @@ export function setCookie(name, value, days) {
     const expirationDate = new Date();
     expirationDate.setDate(expirationDate.getDate() + days);
     document.cookie = `${name}=${value}; path=/; expires=${expirationDate.toUTCString()}`;
+}
+
+/**
+ * Met à jour l'affichage des boutons de connexion et de déconnexion en fonction de l'état de connexion de l'utilisateur.
+ * @param {number} user - L'utilisateur à vérifier.
+ */
+export function updateAuthButtons(user) {
+    const loginButton = document.querySelector(".nav-button[href='/login.html']");
+    const registerButton = document.querySelector(".nav-button[href='/register.html']");
+    const logoutButton = document.querySelector(".logout-button");
+    if (user !== -1) {
+        // L'utilisateur est connecté
+        if (loginButton) loginButton.style.display = 'none';
+        if (registerButton) registerButton.style.display = 'none';
+        if (logoutButton) logoutButton.style.display = 'block';
+    } else {
+        // L'utilisateur n'est pas connecté
+        if (loginButton) loginButton.style.display = 'block';
+        if (registerButton) registerButton.style.display = 'block';
+        if (logoutButton) logoutButton.style.display = 'none';
+    }
 }
